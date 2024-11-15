f56923ef73f9958a5a403ed36f086e709e4a5ebf
Ivan Martinez Cañero <ivanmartinezca10@gmail.com>
2024-11-15 10:40:40 +0100

    [General] {Added} Optimized and ofuscated code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,7 +92,12 @@ android {
     }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
